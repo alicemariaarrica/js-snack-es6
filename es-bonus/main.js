538f67e863348c88c6105a8785053e458c9cf98a
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-13 22:10:32 +0200

    declaration-var-function-start-commit

---
+++ es-bonus/main.js
@@ -0,0 +1,15 @@
+//Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+
+
+ let arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let a = 0.42;
+
+let b = 0.61;
+
+
+function estimateNum (a, b){
+
+}
