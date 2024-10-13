8c9c462e9ca9a959833688bd5ba8abc9f7564f9a
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-13 21:49:28 +0200

    es-3-array-commit

---
+++ es-3/main.js
@@ -0,0 +1,27 @@
+//Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+let arrSquadre = [
+
+    {
+        "nome": "milan",
+        "punti fatti": 30,
+        "falli subiti": 15,
+    },
+
+    {
+        "nome": "inter",
+        "punti fatti": 10,
+        "falli subiti": 10,
+    },
+
+    {
+        "nome": "juve",
+        "punti fatti": 3,
+        "falli subiti": 5,
+    }
+]
+
+//console.log(arrSquadre);
+
+
