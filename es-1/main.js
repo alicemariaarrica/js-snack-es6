8dc566c0886fa93aacb5f3aa86a3a725ae2a6919
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-13 14:53:04 +0200

    final-commit

--- es-1/main.js
+++ es-1/main.js
@@ -23,7 +23,7 @@ let arrbici = [
 ]
 
 
-let bicileggera = arrbici[0]; //Ha 3 scopi: 1- Serve da scheda di memoria come bici con peso minore.
+let biciLeggera = arrbici[0]; //Ha 3 scopi: 1- Serve da scheda di memoria come bici con peso minore.
                                            //2- Subisce un aggiornamento ogni volta 
                                            //che viene rilevata una bici con peso minore.
                                            //3- Inizializzare una variabile con il primo elemento dell'array (arrbici[0]) 
@@ -32,10 +32,11 @@ let bicileggera = arrbici[0]; //Ha 3 scopi: 1- Serve da scheda di memoria come b
 
 
 for (let i = 0; i < arrbici.length; i++) { //ciclo for che serve solo per iterare
-    let bicidaconfrontare = arrbici[i]; // mi permette di accedere ad ogni nuovo elemento dell'array durante ogni nuova iterazione. Quindi biciAttuale rappresenta la bici (oggetto) con cui dovrò in seguito fare un confronto.
+    let biciDaConfrontare = arrbici[i]; // mi permette di accedere ad ogni nuovo elemento dell'array durante ogni nuova iterazione. Quindi biciAttuale rappresenta la bici (oggetto) con cui dovrò in seguito fare un confronto.
 
-    if (bicidaconfrontare.peso < biciLeggera.peso) {
-        biciLeggera = bicidaconfrontare; // Aggiorno la biciLeggera se trova una bici più leggera
+    if (biciDaConfrontare.peso < biciLeggera.peso) {
+        biciLeggera = biciDaConfrontare; // Aggiorno la biciLeggera se trova una bici più leggera
     }
 }
 
+console.log(biciLeggera);
