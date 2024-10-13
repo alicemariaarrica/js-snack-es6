abc068515ac25b8d9a029802c3eca1051a9b81c3
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-13 13:45:18 +0200

    es-in classe-commit

---
+++ es-in-classe/main.js
@@ -0,0 +1,33 @@
+//crea un array di zucchine oggetto indicandone nome e peso e calcola la somma del loro peso
+
+let arrZucchine = [
+
+    {
+        "nome": "zucchina 1",
+        "peso": 30
+    },
+
+    {
+        "nome": "zucchina 2",
+        "peso": 40
+    },
+
+    {
+        "nome": "zucchina 3",
+        "peso": 60
+    }
+]
+
+
+
+
+let sum = 0;
+
+for (let i = 0; i < arrZucchine.length; i++) { //arrZucchine.length serve a garantire che il ciclo si ripeta esattamente per tutti gli elementi dell'array.
+    const zucchina = arrZucchine[i];
+
+    sum = sum + zucchina.peso
+    
+}
+
+console.log(sum);
