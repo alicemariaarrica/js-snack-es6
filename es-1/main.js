471db36aa569269a8de1c15268891c85e90db2b8
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-13 14:49:17 +0200

    ciclo-for-if-comments-commit

--- es-1/main.js
+++ es-1/main.js
@@ -7,21 +7,35 @@
 let arrbici = [
 
     {
-        "nome": "bici 1",
+        "nome": "bici 0",
         "peso": 30
     },
 
     {
-        "nome": "bici 2",
+        "nome": "bici 1",
         "peso": 40
     },
 
     {
-        "nome": "bici 3",
+        "nome": "bici 2",
         "peso": 60
     }
 ]
 
 
-let minibici = arrbici[0];
+let bicileggera = arrbici[0]; //Ha 3 scopi: 1- Serve da scheda di memoria come bici con peso minore.
+                                           //2- Subisce un aggiornamento ogni volta 
+                                           //che viene rilevata una bici con peso minore.
+                                           //3- Inizializzare una variabile con il primo elemento dell'array (arrbici[0]) 
+                                           //serve per avere un punto di partenza nel processo di confronto dei pesi.
+
+
+
+for (let i = 0; i < arrbici.length; i++) { //ciclo for che serve solo per iterare
+    let bicidaconfrontare = arrbici[i]; // mi permette di accedere ad ogni nuovo elemento dell'array durante ogni nuova iterazione. Quindi biciAttuale rappresenta la bici (oggetto) con cui dovrò in seguito fare un confronto.
+
+    if (bicidaconfrontare.peso < biciLeggera.peso) {
+        biciLeggera = bicidaconfrontare; // Aggiorno la biciLeggera se trova una bici più leggera
+    }
+}
 
